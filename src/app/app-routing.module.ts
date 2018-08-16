31fa1b90eba71d0dba5a05e088700facee4be401
Danail Videv <danailvidev@gmail.com>
2018-08-16 15:54:50 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'create', loadChildren: './create/create.module#CreatePageModule' },
+  { path: 'detail', loadChildren: './detail/detail.module#DetailPageModule' },
+  { path: 'edit', loadChildren: './edit/edit.module#EditPageModule' },
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase';
 
+const config = {
+  apiKey: 'AIzaSyAEZQ0A--l8GlUQJ4wo5B4DzHywmJbCLuI',
+  authDomain: 'bulletin-1d070.firebaseio.com',
+  databaseURL: 'https://bulletin-1d070.firebaseio.com/',
+  projectId: 'bulletin-1d070',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+};
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -22,5 +30,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
